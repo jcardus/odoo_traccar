f74ff3b83ea669198c22ebce96654dfb047ce12b
jcardus <asklocation.net@gmail.com>
2025-02-23 20:23:05 +0000

    notification

--- static/src/frotaweb.js
+++ static/src/frotaweb.js
@@ -30,6 +30,34 @@ class OdooTraccarReports extends Component {
     }
 }
 
+class OdooTraccarReports extends Component {
+    static template = "frotaweb.dashboard";
+    setup() {
+        this.state = useState({iframeSrc: ""});
+        this.rpc = useService('rpc')
+        onMounted(async () => {
+            const token = await this.rpc('/odoo_traccar/token')
+            this.state.iframeSrc = `https://${window.location.hostname.replace(/^[^.]+/, "traccar")}/reports/combined?token=${token}`;
+        });
+    }
+}
+
+export function displayNotificationAction(env, action) {
+    const params = action.params || {};
+    const options = {
+        className: params.className || "",
+        sticky: params.sticky || false,
+        title: params.title,
+        type: params.type || "info",
+    };
+    const links = (params.links || []).map((link) => {
+        return `<a href="${escape(link.url)}" target="_blank">${escape(link.label)}</a>`;
+    });
+    const message = markup(sprintf(escape(params.message), ...links));
+    env.services.notification.add(message, options);
+    return params.next;
+}
+
 registry.category("actions").add("odoo_traccar.dashboard", OdooTraccar);
 registry.category("actions").add("odoo_traccar.reports", OdooTraccarReports);
-registry.category("actions").add("odoo_traccar.platform", async () => window.open(`https://${window.location.hostname.replace(/^[^.]+\./, "")}`));
+registry.category("actions").add("odoo_traccar.platform", displayNotificationAction);
